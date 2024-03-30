bd3183693a05c884e9a9dfa019469cdba01e4fa5
Akshay kumar singh <akshaysing975@gmail.com>
2024-03-30 23:18:35 +0530

    first commit

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: ["https://stack-overflow-prod.netlify.app/"],
-    // method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://stack-overflow-prod.netlify.app/"],
+//     method: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
 
-// app.get('https://stack-overflow-prod.netlify.app/',(req, res) => {
-//     res.send("This is a stack overflow clone API")
-// })
+app.get('https://stack-overflow-prod.netlify.app/',(req, res) => {
+    res.send("This is a stack overflow clone API")
+})
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
