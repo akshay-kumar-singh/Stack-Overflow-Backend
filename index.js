05795bf40b76b82ed7ed82dcb6ce77a7ff243a29
Akshaya Singh <akshaysing975@gmail.com>
2026-02-15 10:44:39 +0530

    fix some issue

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ const app = express();
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://stack-overflow-prod.netlify.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+// ADD THIS LINE
+app.options("*", cors());
+
 
 app.get("/", (req, res) => {
   res.send("This is a stack overflow clone API");
