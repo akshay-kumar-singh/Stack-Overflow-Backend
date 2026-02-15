6b91023857a545d28d4d5779dd61e9134bd9aba3
Akshaya Singh <akshaysing975@gmail.com>
2026-02-15 10:14:43 +0530

    fixed issues

--- index.js
+++ index.js
@@ -9,31 +9,19 @@ import connectDB from "./connectMongoDb.js";
 
 dotenv.config();
 connectDB();
+
 const app = express();
-app.use(express.json({ limit: "30mb", extended: true }));
+
+app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: ["https://stack-overflow-prod.netlify.app/"],
-//     method: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-
-app.get('https://stack-overflow-prod.netlify.app/',(req, res) => {
-    res.send("This is a stack overflow clone API")
-})
+app.get("/", (req, res) => {
+  res.send("This is a stack overflow clone API");
+});
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-// const DATABASE_URL = process.env.CONNECTION_URL
-
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+export default app;   // ✅ VERY IMPORTANT
